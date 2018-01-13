4430ef5f29c060489c1ec3e12cce82df4da5639e
jiangzhongbo <jiangzhongbo@gmail.com>
2018-01-13 20:14:33 +0800

    init

---
+++ CMakeFiles/xmr-stak-backend.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/backend/backendConnector.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/backendConnector.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/backend/cpu/crypto/cryptonight_common.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/crypto/cryptonight_common.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/backend/cpu/hwlocMemory.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/hwlocMemory.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/backend/cpu/jconf.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/jconf.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/backend/cpu/minethd.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/minethd.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/backend/globalStates.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/globalStates.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/http/httpd.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/http/httpd.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/http/webdesign.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/http/webdesign.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/jconf.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/jconf.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/misc/console.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/console.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/misc/executor.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/executor.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/misc/telemetry.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/telemetry.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/misc/utility.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/utility.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/net/jpsock.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/net/jpsock.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/net/socket.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/net/socket.cpp.o"
+  "/Users/jzb/workspaces/miner/xmr-stak/xmrstak/version.cpp" "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/version.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "BACKEND_TYPE=cpu"
+  "CONF_NO_CUDA"
+  "CONF_NO_OPENCL"
+  "GIT_BRANCH=master"
+  "GIT_COMMIT_HASH=2ae7260"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/jzb/workspaces/miner/xmr-stak/CMakeFiles/xmr-stak-c.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "/usr/local/include"
+  "/usr/local/opt/openssl/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
